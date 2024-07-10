b083ee829feeaaf7bdd4100eb24106a1c5b68835
VadiMayer <h4998998482@gmail.com>
2024-07-10 16:56:07 +0300

    fixing bugs

--- src/test/java/com/example/springjdbc/controller/BookControllerTest.java
+++ src/test/java/com/example/springjdbc/controller/BookControllerTest.java
@@ -9,11 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -22,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Sql(scripts = "/initDB.sql")
 class BookControllerTest {
 
 
@@ -34,12 +37,11 @@ class BookControllerTest {
     private ObjectMapper objectMapper;
 
     @BeforeEach
-    @Sql(scripts = "/initDB.sql")
     void setUp() {
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         for (int i = 0; i < 3; i++) {
-            bookRepository.save(new Book(i + 1, "title" + i + 1, "author" + i + 1, LocalDate.of(2024, 7, 9 - i)));
+            bookRepository.save(new Book(i + 1, "title" + (i + 1), "author" + (i + 1), LocalDate.of(2024, 7, 9 - i)));
         }
     }
 
